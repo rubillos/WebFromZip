da33a1c6da1e0182d10a14d448aacb717f350578
Randy Ubillos <randy@mac.com>
2025-03-17 09:46:38 -0700

    Initial Commit

---
+++ RickAndRandy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndRandy
+//
+//  Created by Randy on 3/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndRandy/RickAndRandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndRandyApp.swift
+//  RickAndRandy
+//
+//  Created by Randy on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndRandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
